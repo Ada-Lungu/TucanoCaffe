f8e163a9bc55e79c0479c770f89fc7c873b43a02
Ada Lungu <ada.lungu@gmail.com>
2016-09-06 23:30:52 +0200

    added send message implicit intent

--- app/src/main/java/com/example/ada/tucanocaffe/TopLevelActivity.java
+++ app/src/main/java/com/example/ada/tucanocaffe/TopLevelActivity.java
@@ -3,11 +3,14 @@ package com.example.ada.tucanocaffe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.design.widget.FloatingActionButton;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class TopLevelActivity extends Activity {
@@ -17,24 +20,34 @@ public class TopLevelActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
 
+        FloatingActionButton myFab = (FloatingActionButton)findViewById(R.id.fab);
+        myFab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onShareClick(v);
+            }
+        });
+        myFab.setOnHoverListener(new View.OnHoverListener() {
+            public boolean onHover(View v, MotionEvent m) {
+                Toast t =  Toast.makeText(getApplicationContext(), "Shareeeeee....", Toast.LENGTH_SHORT);
+                t.show();
+                return true;
+            }
+        });
+
     }
 
     public void onSeeMenu(View view){
-
-            Intent intent = new Intent (this, MenuOptionsActivity.class);
-            startActivity(intent);
+        Intent intent = new Intent (this, MenuOptionsActivity.class);
+        startActivity(intent);
     }
 
-    public void onShareClick(){
-        Intent intent=new Intent(android.content.Intent.ACTION_SEND);
+    public void onShareClick(View view) {
+        Intent intent = new Intent(android.content.Intent.ACTION_SEND);
         intent.setType("text/plain");
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
 
-// Add data to the intent, the receiving app will decide what to do with it.
+        // Add data to the intent, the receiving app will decide what to do with it.
         intent.putExtra(Intent.EXTRA_SUBJECT, "lalal");
-        intent.putExtra(Intent.EXTRA_TEXT, "hey hey");
-
+        intent.putExtra(Intent.EXTRA_TEXT, "I'm ");
+        startActivity(Intent.createChooser(intent, "How do you want to share?"));
     }
-
-
 }
